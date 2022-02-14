b617aafaf4df1a21a028a1d164e54a8a4452f59f
Shadeslayer10 <95581967+Shadeslayer10@users.noreply.github.com>
2022-02-14 15:47:50 +0530

    Add files via upload

--- commands/Configuration/spawns.js
+++ commands/Configuration/spawns.js
@@ -1,1 +1,16 @@
+module.exports = {
+  name: 'spawns',
+  run: async (client, message, args) => {
+    if (args[0].toLowerCase() === 'small') {
+      if (!message.guild)
+        return message.channel.send(client.config.errors.guildOnlyCommand);
 
+      client.commands.get('spawns-small').run(client, message, args);
+    } else if (args[0].toLowerCase() === 'big') {
+      if (!message.guild)
+        return message.channel.send(client.config.errors.guildOnlyCommand);
+
+      client.commands.get('spawns-big').run(client, message, args);
+    } else return;
+  },
+};
